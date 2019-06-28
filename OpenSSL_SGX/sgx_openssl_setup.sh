7390d51e5ef66f8897b39c20f3b1410dcfbd03dc
JuHyeng Han <sparkly9399@gmail.com>
2019-06-28 11:20:50 +0900

    Fix OpenSSL-SGX build script

--- OpenSSL_SGX/sgx_openssl_setup.sh
+++ OpenSSL_SGX/sgx_openssl_setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 perl Configure -g linux-x86_64 -fPIC no-ui no-idea no-mdc2 no-rc5 no-rc4 no-ec2m no-camellia no-cast no-srp no-hw no-dso no-shared no-stdio no-afalgeng -D_FORTIFY_SOURCE=2 -DGETPID_IS_MEANINGLESS
 sed -i 's/build_all: build_libs build_apps build_tests/build_all: build_libs/g' Makefile
+ln -sf ../../crypto/ui/ui_compat.h include/openssl/ui_compat.h
+ln -sf ../../crypto/ui/ui.h include/openssl/ui.h
